fa56a25629751c7e7b7a8a53d454e32d1b488224
Cenriquez16 <110339860+Cenriquez16@users.noreply.github.com>
2023-05-31 11:03:08 -0500

    Backend

---
+++ src/config.js
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+config();
+
+export default {
+  port: process.env.PORT || 4000,
+  dbUser: process.env.DB_USER || "adminsql",
+  dbPassword: process.env.DB_PASSWORD || "Admin1234",
+  dbServer: process.env.DB_SERVER || "sqlrivera.database.windows.net",
+  dbDatabase: process.env.DB_DATABASE || "",
+  dbPort: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 1433,
+  // dbPort: process.env.DB_PORT || 1433,
+  dbDialect: process.env.DB_DIALECT || "mssql",
+  dbDialectOptions: {
+    instanceName: process.env.DB_DIALECT_OPTIONS_INSTANCE_NAME || "SQLDEV",
+  }
+};
