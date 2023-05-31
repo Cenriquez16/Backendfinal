241151894b9b6f91a47bef1b95919d46588bdd20
Cenriquez16 <110339860+Cenriquez16@users.noreply.github.com>
2023-05-31 14:57:01 -0500

    actualizacion2

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export default {
   dbUser: process.env.DB_USER || "adminsql",
   dbPassword: process.env.DB_PASSWORD || "Admin1234",
   dbServer: process.env.DB_SERVER || "sqlrivera.database.windows.net",
-  dbDatabase: process.env.DB_DATABASE || "",
+  dbDatabase: process.env.DB_DATABASE || "rivera",
   dbPort: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 1433,
   // dbPort: process.env.DB_PORT || 1433,
   dbDialect: process.env.DB_DIALECT || "mssql",
